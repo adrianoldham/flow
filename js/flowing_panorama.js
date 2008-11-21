13f1447450ea2166c81247b9067c1f25ee45d64c
Edmond Leung <leung.edmond@gmail.com>
2008-11-21 11:27:38 +0000

    Added the new panorama features to flowingpanorama.

--- js/flowing_panorama.js
+++ js/flowing_panorama.js
@@ -1,13 +1,32 @@
 var FlowingPanorama = Class.create({
     initialize: function(flowContainer, panoramaContainer, selector, flowOptions, panoramaOptions) {
+        panoramaOptions = panoramaOptions || {};
+        
         this.panoramaContainer = $(panoramaContainer);
         this.flowContainer = $(flowContainer);
         
         if (this.panoramaContainer == null || this.flowContainer == null) return;
         
+        // if auto play is off, make sure panorama doesn't change
+        if (!panoramaOptions.autoPlay) {
+            panoramaOptions.scrollSpeed = 0;
+            panoramaOptions.showPauseIndicator = false;
+        }
+        
+        // disable autoscroll in flowing panorama if panorama has auto play already
+        if (panoramaOptions.autoPlay) {
+            flowOptions.autoScroll = false;
+        }
+        
+        // sets auto play delay
+        if (panoramaOptions.autoPlayDelay) {
+            panoramaOptions.minDelay = panoramaOptions.autoPlayDelay;
+        }
+        
         this.anchors = $$(selector);
         
         if (this.anchors.length == 0) return;
+        if (this.anchors.length <= 1) panoramaOptions.showPauseIndicator = false;
 
         this.flow = new Flow(this.flowContainer, selector, Object.extend(flowOptions || {}, {
             onFocus: function(element) {
